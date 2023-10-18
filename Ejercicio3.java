b80d28dc3f574e4a2cbac488dc28a84cdbb2e152
cgilsanchez <cgilsanchez.02@gmail.com>
2023-10-18 10:47:28 +0200

    First commit

---
+++ Ejercicio3.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca un dia de la semana del 1-7:");
+        int dia = sc.nextInt();
+        switch(dia){
+            case 1:
+            System.out.println("Lunes");
+            break;
+            case 2:
+            System.out.println("Martes");
+            break;
+            case 3:
+            System.out.println("Miercoles");
+            break;
+            case 4:
+            System.out.println("Jueves");
+            break;
+            case 5:
+            System.out.println("Viernes");
+            break;
+            case 6:
+            System.out.println("Sabado");
+            break;
+            case 7:
+            System.out.println("Domingo");
+            break;
+
+            default:
+            System.out.println("No ha introducido bien el dia de la semana");
+        }
+
+        sc.close();
+    }
+}
